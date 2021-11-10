289d1deb2f6499368538655aa5e6712e26bc236f
Mark Movh <mark.movh@stud.noroff.no>
2021-11-10 01:29:41 +0100

    checks added to ensure inputs are integers

--- Github.py
+++ Github.py
@@ -4,11 +4,14 @@ class RandomNumbers:
     def __init__(self):
         print("Hello! This programm will find you random numbers")
 
-        self.amount = int(input("Submit how many numbers you want returned: "))
-        self.minrange = int(input("What is the lowest number you want? "))
-        self.maxrange = int(input("What is the highest range you want? "))
+        try:
+            self.amount = int(input("Submit how many numbers you want returned: "))
+            self.minrange = int(input("What is the lowest number you want? "))
+            self.maxrange = int(input("What is the highest range you want? "))
 
-        self.get_random_numbers(self.amount, self.minrange, self.maxrange)
+            self.get_random_numbers(self.amount, self.minrange, self.maxrange)
+        except:
+            print("Error all input should be an integer. Try again.")     
 
     def get_random_numbers(self, amount, minrange, maxrange):
         for number in range(amount):
